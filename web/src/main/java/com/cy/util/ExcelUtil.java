7a0690d2d155241355fb9399b927990f7c1c519f
zjls007 <zjls007@qq.com>
2017-03-14 14:07:29 +0800

    Signed-off-by: zjls007 <zjls007@qq.com>

---
+++ web/src/main/java/com/cy/util/ExcelUtil.java
@@ -0,0 +1,22 @@
+package com.cy.util;
+
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+/**
+ * Created by zxj on 2017/3/13.
+ */
+public class ExcelUtil {
+
+    public HSSFWorkbook createExcel() {
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet();
+        sheet.setColumnWidth(1, 10000);
+        HSSFRow row = sheet.createRow(1);
+        row.setHeightInPoints(23);
+
+        return workbook;
+    }
+
+}
