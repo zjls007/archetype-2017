c401fb47a17e0b91103fe03218ec21602b9671ee
zjls007 <zjls007@qq.com>
2017-04-28 13:51:37 +0800

    Signed-off-by: zjls007 <zjls007@qq.com>

--- src/test/java/basic/JunitSpringContext.java
+++ src/test/java/basic/JunitSpringContext.java
@@ -8,18 +8,19 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import javax.annotation.Resource;
+
 /**
  * Created by zxj on 2016/12/14.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
 @ContextConfiguration(
-        {"file:web/src/main/webapp/WEB-INF/config/spring-application-*.xml",
-         "file:src/main/webapp/WEB-INF/config/spring-application-*.xml"})
+        {"file:src/main/webapp/WEB-INF/config/spring-application-*.xml"})
 //@ContextConfiguration({"classpath:spring/spring-application-*.xml"})
 public class JunitSpringContext {
 
-    @Autowired
+    @Resource
     private UserInfoDAO userInfoDAO;
 
     @Test
