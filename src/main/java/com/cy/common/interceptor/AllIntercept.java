a2f04bf03256d7957453ca29d02898922b26075f
zjls007 <zjls007@qq.com>
2017-10-19 10:49:59 +0800

    修复错误

--- src/main/java/com/cy/common/interceptor/AllIntercept.java
+++ src/main/java/com/cy/common/interceptor/AllIntercept.java
@@ -1,14 +1,7 @@
 package com.cy.common.interceptor;
 
-import com.cy.common.constant.Constants;
-import com.cy.common.util.WebUtil;
-import org.apache.shiro.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,6 +15,7 @@ public class AllIntercept implements HandlerInterceptor {
 
     public Logger logger = LoggerFactory.getLogger(AllIntercept.class);
 
+    @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object handler) throws Exception {
         String scheme = httpServletRequest.getScheme();
         String serverName = httpServletRequest.getServerName();
@@ -35,11 +29,14 @@ public class AllIntercept implements HandlerInterceptor {
         return true;
     }
 
+    @Override
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
 
     }
 
+    @Override
     public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
 
     }
+
 }
