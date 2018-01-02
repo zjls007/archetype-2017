c1eda26341d67ae2bc96c67f24c20501e52bc651
zjls007 <zjls007@qq.com>
2018-01-02 10:16:56 +0800

    AES加密

--- src/main/java/com/cy/common/util/AESUtil.java
+++ src/main/java/com/cy/common/util/AESUtil.java
@@ -1,5 +1,8 @@
 package com.cy.common.util;
 
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
@@ -13,22 +16,52 @@ public class AESUtil {
 
     /**
      * 加密
-     *http://blog.csdn.net/hbcui1984/article/details/5201247
-     * @param content 需要加密的内容
-     * @param password  加密密码
+     * http://blog.csdn.net/hbcui1984/article/details/5201247
+     *
+     * @param content  需要加密的内容
+     * @param password 加密密码
      * @return
      */
-    public static byte[] encrypt(String content, String password) throws Exception {
-        KeyGenerator kgen = KeyGenerator.getInstance("AES");
-        kgen.init(128, new SecureRandom(password.getBytes()));
-        SecretKey secretKey = kgen.generateKey();
-        byte[] enCodeFormat = secretKey.getEncoded();
-        SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
-        Cipher cipher = Cipher.getInstance("AES");// 创建密码器
-        byte[] byteContent = content.getBytes("utf-8");
-        cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
-        byte[] result = cipher.doFinal(byteContent);
-        return result; // 加密
+    public static String encrypt(String content, String password) {
+        try {
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(128, new SecureRandom(password.getBytes()));
+            SecretKey secretKey = kgen.generateKey();
+            byte[] enCodeFormat = secretKey.getEncoded();
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
+            byte[] byteContent = content.getBytes("utf-8");
+            cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
+            byte[] result = cipher.doFinal(byteContent);
+            BASE64Encoder encoder = new BASE64Encoder();
+            return encoder.encode(result); // 加密
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 解密
+     *
+     * @param content  待解密内容
+     * @param password 解密密钥
+     * @return
+     */
+    public static String decrypt(String content, String password) {
+        try {
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(128, new SecureRandom(password.getBytes()));
+            SecretKey secretKey = kgen.generateKey();
+            byte[] enCodeFormat = secretKey.getEncoded();
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+            Cipher cipher = Cipher.getInstance("AES");// 创建密码器
+            cipher.init(Cipher.DECRYPT_MODE, key);// 初始化
+            BASE64Decoder decoder = new BASE64Decoder();
+            byte[] result = cipher.doFinal(decoder.decodeBuffer(content));
+            return new String(result, "utf-8"); // 加密
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
